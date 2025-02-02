bffc93b1152c79c1c36b0ea63397605d5ce4ad1c
Louis Bertrand <louis@bertrandtech.ca>
2025-02-02 18:14:26 -0500

    Completed, tested on Linux.

--- multi-clone.py
+++ multi-clone.py
@@ -30,10 +30,11 @@ Throughout, maintain a list of entries with success/fail status
 
 '''
 from sys import argv
-import re
-import pathlib
+import re  # Regular expressions to dig through folder names
+import pathlib  # file & folder path objects
 import os
-from datetime import datetime
+from datetime import datetime  # The strptime() method to convert strings to date-time
+import subprocess  # for the git clone task
 
 # Constants
 server_name = "git@github-fleming"  # Different from my usual GitHub ID for SSH key reasons
@@ -50,7 +51,7 @@ def get_folder(argv):
     return folder.resolve(strict=True)
 
 def list_folders(folder):
-    '''Return a list of folders as Path objects.
+    '''Return a list of folders as pathlib.Path objects.
     Parameter folder is a Path containing the target folders.
     '''
     folders = []
@@ -61,53 +62,104 @@ def list_folders(folder):
     return folders
 
 def get_datetime(grps):
-   hh = int(grps[5])//100
-   mm = int(grps[5])%100
-   timestamp = datetime.strptime(f'{grps[2]} {grps[3]} {grps[4]} {hh} {mm} {grps[6]}', r'%b %d %Y %I %M %p')
-   return timestamp
+    '''The groups returned by the regex are:
+        MON DD YYYY HHMM AM
+        HHMM could also be HMM, hence the divide by 100 and modulo 100,
+        AM could be PM; this is where strptime() very convenient
+        (so D2L doesn't know about 24-hour time?).
+    return a datetime instance.
+    '''
+    hh = int(grps[5])//100
+    mm = int(grps[5])%100
+    timestamp = datetime.strptime(f'{grps[2]} {grps[3]} {grps[4]} {hh} {mm} {grps[6]}', r'%b %d %Y %I %M %p')
+    return timestamp
 
 def extract_student_info(path):
+    '''Using a regex, break up the folder name produced by the D2L downloader into groups.
+        NNNNN-MMMMMM - First Last - MON DD, 2025 HHMM PM
+    return a dictionary of student information, or None if the regex did not match anything.
+    '''
+    # Extract the last component of the path, the actual folder name.
     folder_name = path.name
-    # print(folder_name)
     pattern = r'\d+-\d+\s-\s(\w+) (\.|\w+|\w+-\w+)\s-\s(\w+)\s(\d+),\s(\d{4})\s(\d{3,4})\s(AM|PM)$'
 
     mat = re.search(pattern, folder_name)
     if mat:
         grps = mat.groups()
         timestamp = get_datetime(grps)
-        # print(grp)
         student = {
             'folder': path,
             'datetime': timestamp,
             'first': grps[0],
             'last': grps[1],
+        # the next two will be added later, from the HTML file in the folder.
         #     'userid': <github user ID>,
         #     'repo': <reponame>,
-            'status': 'folder'
+            'status': 'folder'  # Progress indicator, means "have folder"
         }
         return student
     else:
-        return None
+        return None  # regex did not match
 
 def get_github_info(students):
-    '''Add GitHub info to each dictionary.'''
+    '''Add GitHub info to each dictionary.
+    Look for the .html file created by the D2L downloader. This file contains
+    the comment that should give the URL to the repo on GitHub.
+    Adds user ID and repo name to the student's dictionary, 
+    then forms the proper URL to clone using ssh (instead of https).
+    returns None
+    '''
     for student in students.values():
         folder = student['folder']
-        # Get the .html file 
-        for name in folder.glob('*.html'):
+        # Get the .html file(s) 
+        for name in folder.glob('*.html'): 
             with open(name, 'r') as html:
                 for line in html:
                     mat = re.search(r"href=\"(.+)\"", line)
                     if mat:
                         url = mat.group(1)
-                        components = url.split('/')
+                        components = url.split('/')  # split along / separator
                         student['userid'] = components[3]
                         student['repo'] = components[4]
                         # Form the SSH URL 
                         # Example: git clone git@github-fleming:CSIkid/COMP593-lab2.git
                         student['ssh_url'] = f"{server_name}:{components[3]}/{components[4]}.git"
-                        print(student)
+                        student['status'] = 'github'  # Means that we have the GitHub info.
     return
+
+def clone_repos(students):
+    '''Spawn a new task for each to git clone the student repo.'''
+    for k in students.keys():
+        student = students[k]
+        command = ("git", "clone", student['ssh_url'])
+        print(command)
+        try:
+            completed = subprocess.run( command,
+                               stdin=None,
+                               input=None,
+                               stdout=None,
+                               stderr=None,
+                               capture_output=True,
+                               shell=False,
+                               cwd=student['folder'],
+                               timeout=None,
+                               check=False,
+                               encoding=None,
+                               errors=None,
+                               text=True,
+                               env=None,
+                               universal_newlines=None
+                            )
+            print(f'{k}: return={completed.returncode}')
+            if completed.returncode == 0:
+                student['status'] = 'cloned'
+            else:
+                student['status'] = 'problem'
+                print(f'  stdout="{completed.stdout}"\n  stderr="{completed.stderr}"')
+        except subprocess.CalledProcessError as err:
+            print(f'Subprocess failed for student {student['first']} {student['last']}\n'
+                  '{err.cmd}: {err.output}')
+    return  # return nothing, everything is in the dictionary
 
 def main():
     try:
@@ -116,17 +168,25 @@ def main():
         print('multi_clone: Cannot resolve directory name from command line')
         print(err)
         exit(0)
-    print(main_folder)
+    print(f'[{datetime.now().isoformat()}] multi-clone.py: Processing {main_folder}')
     folders = list_folders(main_folder)
     students = {}
     for f in folders:
         student = extract_student_info(f)
+        # Add the student dictionary to the dictionary of students,
+        # using first+last as the key.
         if student:
-            students[student['first']+student['last']] = student
+            students[student['first'].lower() + student['last'].lower()] = student
+
     get_github_info(students)  # Add GitHub info to each dictionary
-
-    clone_repos(students)  # spawn a new task for each
-
+    # for d in students.values():
+    #     print(d)
+    # spawn a new task for each, recording the outcome in the dictionary.
+    clone_repos(students)
+    # Report the outcome
+    for k in students.keys():
+        student = students[k]
+        print(f'{k}: {student['status']}')
     return 0
 
 
